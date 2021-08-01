fb094cbc9ed4a5068081f0e06a03481872189f16
Muchaaaaa <sylviaslyg@gmail.com>
2021-08-01 23:12:58 +0300

    Js file linked

--- js/script.js
+++ js/script.js
@@ -4,5 +4,25 @@ let pizzaTopping=["Pepperoni","Mushroom","Bacon","Extra cheese","Black olives","
  
 
 function getSize(){
-  var pizzaSize=document.getElementById("")
+  var pizzaSize=document.getElementById("pizza-size").nodeValue;
+  return (pizzaSize);
+}  
+function getCrust(){
+  var pizzaCrust=document.getElementById("pizza-crust").nodeValue;
+  return (pizzaCrust);
+} 
+function getToppings(){
+  var pizzaTopping=document.getElementById("pizza-toppings").nodeValue;
+  return (pizzaToppings);
+} 
+function getNumber(){
+  var number=document.getElementById("num").nodeValue;
+  return (nnumber);
 }
+function checkout(){
+  var Total= getCrust() + getSize() + getToppings() * getNumber()+300  ;
+  alert("Your total bill is Ksh." + (Total));
+  prompt("Please enter your location")
+  alert("Thank you and enjoy")
+}   
+ 
